23d479ab7cdf034162f684af953a6d4c336f1701
Rutvik Gautam Chavhan <114423832+Rutuvik@users.noreply.github.com>
2023-07-14 20:17:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.ineuron;
+import java.util.HashSet;
+import java.util.Set;
+
+//WAP to find if String contains all unique character
+public class Main {
+    public static boolean hasUniqueCharacters(String str) {
+
+        Set<Character> uniqueChars = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+
+            if (uniqueChars.contains(ch)) {
+                return false;
+            }
+
+            uniqueChars.add(ch);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input1 = "abcdefg";
+
+        if(hasUniqueCharacters(input1)){
+            System.out.println("Specified Input has unique characters");
+        }
+
+    }
+
+}
